abcc88a4a7f82010d75a855d3362e33091a13e40
Saad A. <saad7@terpmail.umd.edu>
2025-08-04 13:24:53 -0400

    current staging push

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,14 @@
+import {Ratelimit} from "@upstash/ratelimit";
+import {Redis} from "@upstash/redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// create ratelimiter allows 5 req per 20 seconds.
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  // says 100 requests per 60 seconds
+  limiter: Ratelimit.slidingWindow(10, "20 s"),
+})
+
+export default ratelimit;
